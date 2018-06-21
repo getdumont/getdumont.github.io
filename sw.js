854a485153d3177bac206400cf8bed1bf0c21b44
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-06-21 20:40:31 +0000

    Deploy getdumont/getdumont.github.io to github.com/getdumont/getdumont.github.io.git:master

---
+++ sw.js
@@ -0,0 +1,15 @@
+var cacheFiles = [
+    '/manifest.json',
+    '/assets/css/index.css',
+    '/assets/images/logo.png',
+    '/assets/images/logo_blue.png',
+    '/assets/scripts/index.js',
+    'https://fonts.gstatic.com/s/opensans/v15/mem5YaGs126MiZpBA-UN_r8OUuhpKKSTjw.woff2',
+    'https://fonts.gstatic.com/s/opensans/v15/mem8YaGs126MiZpBA-UFVZ0bf8pkAg.woff2'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(caches.open(cacheName).then(function(cache) {
+        return cache.addAll(cacheFiles);
+    }));
+});

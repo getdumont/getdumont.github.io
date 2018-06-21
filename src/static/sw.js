2c180f194038381703563a6907db40542ec4a614
Guilherme Diego <guidiego.expgames@gmail.com>
2018-06-21 17:43:31 -0300

    Add: Manifest & SW

---
+++ src/static/sw.js
@@ -0,0 +1,16 @@
+var cacheName = 'DUMONT_CACHE';
+var cacheFiles = [
+    '/manifest.json',
+    '/assets/css/index.css',
+    '/assets/images/logo.png',
+    '/assets/images/logo_blue.png',
+    '/assets/scripts/index.js',
+    'https://fonts.gstatic.com/s/opensans/v15/mem5YaGs126MiZpBA-UN_r8OUuhpKKSTjw.woff2',
+    'https://fonts.gstatic.com/s/opensans/v15/mem8YaGs126MiZpBA-UFVZ0bf8pkAg.woff2',
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(caches.open(cacheName).then(function(cache) {
+        return cache.addAll(cacheFiles);
+    }));
+});

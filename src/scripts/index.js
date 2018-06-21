bc707f04bff2c069664c492ea22f75af13ae845b
Guilherme Diego <guidiego.expgames@gmail.com>
2018-06-21 15:28:14 -0300

    Add: Async Material

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,10 +8,20 @@ function addClickOnStep(about, button, index) {
 }
 
 window.onload = function () {
+    const lazyImages = document.querySelectorAll("[data-img]");
     const about = document.querySelector('#como-funciona .hero-content');
     const buttons = document.querySelectorAll('.steper .step');
 
     for (let index in Object.keys(buttons)) {
         addClickOnStep(about, buttons[index], parseInt(index) + 1);
+    }
+
+    for (let span of lazyImages) {
+        const img = document.createElement('img');
+        img.src = span.dataset.img;
+        img.alt = span.dataset.alt;
+
+        span.parentNode.appendChild(img);
+        span.parentNode.removeChild(span);
     }
 }

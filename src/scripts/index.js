094d25aa66f4ce4755180c7e894bb5ae0ad0928a
Guilherme Diego <guidiego.expgames@gmail.com>
2018-06-21 17:14:04 -0300

    Add: Slide Stuff

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,20 +1,48 @@
+let slideTimeout = null;
+
+function activeSlide(about, index) {
+    about.classList.remove('slide-1');
+    about.classList.remove('slide-2');
+    about.classList.remove('slide-3');
+    about.classList.add('slide-' + (index + 1));
+}
+
 function addClickOnStep(about, button, index) {
     button.onclick = function () {
-        about.classList.remove('slide-1');
-        about.classList.remove('slide-2');
-        about.classList.remove('slide-3');
-        about.classList.add('slide-' + index);
+        activeSlide(about, index);
+        clearTimeout(slideTimeout);
+        nextSlide(about, index + 1);
     };
 }
 
+function nextSlide(about, index) {
+    slideTimeout = setTimeout(() => {
+        let nextIndex = index == 2 ? 0 : index + 1;
+        activeSlide(about, index);
+        nextSlide(about, nextIndex);
+    }, 19000);
+}
+
 window.onload = function () {
-    const lazyImages = document.querySelectorAll("[data-img]");
+    /**
+     * SLIDE
+     */
     const about = document.querySelector('#como-funciona .hero-content');
-    const buttons = document.querySelectorAll('.steper .step');
+    const steper = document.querySelector('.steper');
+    const buttons = steper.querySelectorAll('.step');
 
     for (let index in Object.keys(buttons)) {
-        addClickOnStep(about, buttons[index], parseInt(index) + 1);
+        addClickOnStep(about, buttons[index], parseInt(index));
     }
+
+
+    nextSlide(about, 0);
+
+
+    /**
+     * LAZY IMAGE
+     */
+    const lazyImages = document.querySelectorAll("[data-img]");
 
     for (let span of lazyImages) {
         const img = document.createElement('img');
